c1bb59bb8dfc7565eaf1fa659fcf70e695adaa0b
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 18:25:00 +0900

    Add files via upload

--- question11.c
+++ question11.c
@@ -1,16 +1,30 @@
-//문제11
-//수식에는 값과 "형"이 있다.
-//수식의 형 관련 >1)자동형변환 2)강제형변환-캐스트연산자
-//수식의 형을 알고 싶을 때는 sizeof연산자를 쓰자
+//문제11.+12
+//for문 루프에서 break문과 continue문의 동작 살펴보기
+/*
 #include <stdio.h>
 int main(void) {
-	char c = 'A';
-
-	printf("sizeof(c) = %u\n", sizeof(c));//char형
-	printf("sizeof('A') = %u\n", sizeof('A'));//**문자상수 자체는 int형이다
-	printf("sizeof(c+c) = %u\n", sizeof(c + c));//int형[정수승격]
-	printf("sizeof(c='A') = %u\n", sizeof(c = 'A'));//char형
-
+	int i = 1;
+	for (putchar('1'); putchar('2'); putchar('3')) {
+		if (++i == 5)
+			break;
+		putchar('4');
+		continue;
+		putchar('5');
+	}
 	return 0;
+}*/
 
+#include <stdio.h>
+int main(void) {
+	putchar('1');
+	putchar('2');
+	int i = 0; 
+	while (1) {
+		putchar('4');
+		putchar('3');
+		putchar('2');
+		if (++i == 3)
+			break;
+	}
+	return 0;
 }
