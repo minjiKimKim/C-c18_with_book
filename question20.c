e97789520dfd9111df3ebc33e3dfc6981138c6c4
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:38:44 +0900

    Add files via upload

--- question20.c
+++ question20.c
@@ -1,28 +1,22 @@
-//문제 20
-//printf 출력결과 관찰
-
-
+//문제20
+//c언어에서 실수형은 범위를 벗어나는 무한대 개념을 수학에서와 같이 사용하기도 한다.
 #include <stdio.h>
-int main(void) {
-	printf("%.2f\n", 12.0 / 5.0 * 5.0);
-	printf("%.2f\n", 12 / 5.0 * 5.0);
-	printf("%.2f\n", 12.0 / 5 * 5.0);
-	printf("%.2f\n", 12 / 5 * 5.0);
+int main(void){
+	float one = 1.0, zero = 0.0, r1, r2;
+
+	r1 = one / zero;
+	r2 = -one / zero;
+
+	printf("무한대 * 무한대 = %f\n", r1 * r1);
+	printf("무한대 * -무한대 = %f\n", r1 * r2);
+	printf("무한대 * 0 = %f\n", r1 * zero);
+	printf("-무한대 * 0 = %f\n", r2 * zero);
+	printf("1/무한대 = %f\n", one / r1);
+	printf("1/-무한대 = %f\n", one / r2);
+	printf("무한대 / 무한대 = %f\n", r1 / r1);
+	printf("무한대/-무한대 = %f\n", r1 / r2);
+	printf("무한대 - 무한대 =%f\n", r1 - r1);
+	printf("무한대 + 1 = %f\n", r1 + one);
 
 	return 0;
-}
-
-/*
-아래 예제는 입력버퍼에 관한 궁금증
-#include <stdio.h>
-int main(void) {
-	int a;
-	float b;
-	char c;
-	
-	scanf("%d %f %c", &a, &b, &c);
-	printf("%d %f %c\n", a, b, c);
-
-	return 0;
-}
-*/
+	}
