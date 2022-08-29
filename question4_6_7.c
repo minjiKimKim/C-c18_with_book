163cfe16161f48db429e57df4eba1f96c4d7bec7
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:11:11 +0900

    Add files via upload

---
+++ question4_6_7.c
@@ -0,0 +1,22 @@
+//문제 4번
+/*#include <stdio.h>
+int main(void) {
+	printf("마라톤의 거리 : %.3f km/n", 1.609344 * (26+385/1760));
+	return 0;
+}*/
+
+//문제 6번 > 16진 상수 궁금증! > 16진상수는 대소문자 혼용 가능[상관없음]
+						//  > %x %X %#x 차이점
+/*#include <stdio.h>
+int main(void) {
+	printf("0xabc = %d  0xaBc = %d  0xABC = %d\n", 0xabc, 0xaBc, 0xABC);
+	printf("0xabc = %x  0xabc = %X  0xabc = %#x\n", 0xabc, 0xabc, 0xabc);
+	return 0;
+}*/
+
+//문제 7번 > 8진수와 16진 상수를 넘어가는 문자를 쓰게 되면??
+/*#include <stdio.h>
+int main(void) {
+	printf("0x6G7 = %d  088 = %d  0123C = %d\n", 0x6G7, 088, 0123C);
+	return 0;
+}*/
