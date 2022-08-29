b405f7f9fa625e75b49c1eb5f3cbd7fd36f84fee
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:36:30 +0900

    Add files via upload

--- question16.c
+++ question16.c
@@ -1,8 +1,10 @@
-%:include <stdio.h>
+//문제16.
+//컴퓨터가 다룰 수 있는 실수는 매우 제한적이라, 실수는 부정확하게 자료형에 담긴다.
+#include <stdio.h>
+int main(void) {
+	double x = 123.45123451234512345;
+	double y = 123.45123451234512300;
 
-int main(void)
-<%
-	printf("%d * %d = %d\n", 2, 3, 2 * 3);
+	printf("%.17f %.17f\n", x, y);
 	return 0;
-
-%>
+}
