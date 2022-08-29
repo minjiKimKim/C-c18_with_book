c6e4d8ea5396ad76e7ade155631aaa52f65ebaf1
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:18:18 +0900

    Add files via upload

--- question16.c
+++ question16.c
@@ -1,12 +1,8 @@
-//2개의 실수를 더한 프로그램을 출력해라
+%:include <stdio.h>
 
-#include <stdio.h>
-int main(void) {
-	float n1, n2;
-	printf("실수를 입력하세요\n");
-
-	scanf("%f %f", &n1, &n2);
-	printf("%.2f와 %.2f의 합 : %.2f", n1, n2, n1+n2);
-
+int main(void)
+<%
+	printf("%d * %d = %d\n", 2, 3, 2 * 3);
 	return 0;
-}
+
+%>
