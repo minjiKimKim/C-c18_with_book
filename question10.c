02515e51d514f97c6e638f6ddf74d2c117db1ae9
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 18:22:38 +0900

    Add files via upload

--- question10.c
+++ question10.c
@@ -1,13 +1,9 @@
-//문제10
-//일반적 산술 변환
+//문제10. control+c 키의 의미
 #include <stdio.h>
-#include <limits.h>
 int main(void) {
-	unsigned char a = 100, b = 200, d;
-	signed char c = -50;
-	d = a + b + c; //int형이 unsigned char형으로[250]
-	printf("d = %d\n", d);
-	printf("UCHAR_MAX : %d\n", UCHAR_MAX);
-	return 0;
+	while (1) {
+		printf("무한루프\n");
 
+	}
+	return 0;
 }
