b91e466172a7bfcfc8a56bd92cca3d16f447c569
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 18:16:24 +0900

    Add files via upload

--- question1.c
+++ question1.c
@@ -1,16 +1,15 @@
-//문제1. sizeof를 이용해 자료형의 크기를 알아내라
+//문제1 > 프로그램을 출력해보고, 실행결과를 비교해봐라.
+//관계연산자, 등가연산자, 논리연산자 모두 수식의 결과값이 0 아니면 1임!!
+//그 중, !연산자[논리연산자]는 변수의 값 자체가 아닌, 수식의 결과를 바꾸는 연산자임을 기억!
+
 #include <stdio.h>
 int main(void) {
-	printf("%d\n", sizeof(_Bool));
-	printf("%d\n", sizeof(char));
-	printf("%d\n", sizeof(short));
-	printf("%d\n", sizeof(int));
-	printf("%d\n", sizeof(unsigned));
-	printf("%d\n", sizeof(long));
-	printf("%d\n", sizeof(long long));
-	printf("%d\n", sizeof(float));
-	printf("%d\n", sizeof(double));
-	printf("%d\n", sizeof(long double));
+	char c = 'A'; //'A'=65
+	int i = 5, j = 10;
+
+	printf("%d %d %d\n", !c, !!c, !!!c);
+	printf("%d %d %d\n", -!i, !- i, !- i - !j);
+	printf("%d %d %d\n", -!i, !- i, !- i - !j);
+	printf("%d %d %d\n", ! (6 * j + i - c), !i - 5, !j - 10);
 	return 0;
 }
-
