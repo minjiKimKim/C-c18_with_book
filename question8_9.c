db51372c8b4525c472f5903d10457ad795ccf87b
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:32:22 +0900

    Add files via upload

--- question8_9.c
+++ question8_9.c
@@ -1,13 +1,20 @@
-/*
-문제 8,9번 같은 형식
-: 각각의 부동형상수[10진상수의 지수형태, 16진상수 형태]의 실수 값은 무엇일까?
-*/
-
+    //문제8. 다음 프로그램을 실행하고, 왜 가장 큰 정수값을 출력하는지 설명해라
+/* >이진수 형태 이해
 #include <stdio.h>
 int main(void) {
-	printf("12e2 : %f\n", 12e2);
-	printf("0x14p-3 : %f\n", 0x14p-3);
+    unsigned long long val = -1;
+    printf("가장 큰 정수 : %llu\n", val);
+    return 0;
+}*/
 
-	return 0;
-
+//문제9. 
+//char형은 1)문자, 2)작은 정수 를 다룰 때 사용될 수 있다.
+//그러나 char형이 담을 수 없이 큰 정수를 담게 되면 어떻게 될까?
+#include <stdio.h>
+#include <limits.h>
+int main(void) {
+    char c1 = 256, c2 = 257;
+    printf("c1 : %d, c2 : %d\n", c1, c2);
+    printf("CHAR_MAX : %d\n", CHAR_MAX);
+    return 0;
 }
