2bb5d77747ba40ac4d276c3c7dedb9faf08dc7be
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:03:16 +0900

    Add files via upload

---
+++ question12.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+	printf("Hello!\nMy name is James Kim\n Nice to meet you. can I pracite something\
+more?");
+	return 0; 
+	//굳이 printf 3번 줄별로 안 써도 됨. 가독성은 떨어지지만
+	//그리고 줄이 넘어가면 \붙이고 엔터하면 그대로 다음줄로 이어짐
+	
+}
