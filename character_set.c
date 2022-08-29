efca8557e41965719f6060da30a4ff5b2e4ce418
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:10:45 +0900

    Add files via upload

---
+++ character_set.c
@@ -0,0 +1,17 @@
+// 문자열 예제
+//앞선 예제 문자와 마찬가지로 문자열은 ""로 이루어진 문자들의 모임!
+
+#include <stdio.h>
+int main(void) {
+	printf("%s %s %s\n", "programming", "is", "fun!!"); 
+	printf("VS\n");
+	printf("%s\n", "programming" "is" "fun!!");
+	printf("VS\n");
+	printf("%s\n", "programming " "is " "fun!!");
+
+	printf("%s\n", "\"C\" programming language \
+is a high_level language.");
+
+	return 0;
+	
+}
