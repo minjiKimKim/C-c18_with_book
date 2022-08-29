083758deb5761e21f8143d2a7b0bb2877c4d0165
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:15:45 +0900

    Add files via upload

--- question12.c
+++ question12.c
@@ -1,9 +1,15 @@
+/*
+문제12 > 이스케이프 시퀀스 문자 출력
+*/
 #include <stdio.h>
 int main(void) {
-	printf("Hello!\nMy name is James Kim\n Nice to meet you. can I pracite something\
-more?");
-	return 0; 
-	//굳이 printf 3번 줄별로 안 써도 됨. 가독성은 떨어지지만
-	//그리고 줄이 넘어가면 \붙이고 엔터하면 그대로 다음줄로 이어짐
-	
+	//printf("'\a'");
+	printf("'\b'\n");
+	printf("abcd\bc\n");
+	printf("what?\f\n");
+	printf("\f\n");
+	printf("ddddddd\ta\vdd\rs\n");
+	printf("aaaa\n");
+	printf("1234563\r722\n");
+	printf("123456\r789\n");
 }
