f34544bd999b37260216305e3f3f78439ca9de37
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 18:25:31 +0900

    Add files via upload

--- question14.c
+++ question14.c
@@ -1,12 +1,17 @@
-//문제14. 화씨온도(F)를 입력받아 섭씨온도(C)로 출력하는 프로그램 작성
+/*
+문제 14
+변수 이름과 레이블 이름을 같이 쓴다면?
+*/
 
 #include <stdio.h>
 int main(void) {
-	float C, F;
-	printf("화씨온도를 입력하세요: \n");
-	scanf("%f", &F );
-
-	C = (F - 32) * 5 / 9;
-	printf("%.2f = %.2f\n",F, C);
+	int i;
+	for (i = 0; i < 10; ++i) {
+		if (i == 5)
+			goto i;
+		printf("for : i = %d\n", i);
+	}
+i:
+	printf("i = %d\n", i);
 	return 0;
 }
