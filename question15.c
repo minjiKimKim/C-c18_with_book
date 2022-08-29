057944e6716b4e2c9e898af7882ce89ac9410aca
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-08-29 16:17:53 +0900

    Add files via upload

--- question15.c
+++ question15.c
@@ -1,12 +1,31 @@
-//거듭제곱 프로그램
-
+//아래의 1번처럼 작성해도 세미콜론으로 토큰을 잘 나누기만 하면 실행은 된다. 
+//그러나, 가독성이 좋지 않아 2번처럼 바꾸는 것이 현명하다.
+/*1번
 #include <stdio.h>
 int main(void) {
-	int num;
-	printf("정수를 입력하세요.\n");
-	scanf("%d", &num);
+	int a; int
+		b; float
+		c; a = 0; c
+		= 3.14; printf
+		("Input a(integer value) : "
+		); scanf("%d",
+			&a); printf("Radius : %d\n", a); b = 2
+		* a; printf("Diameter : %d\n", b); 
+	printf("Circumstance : %.2f\n", b * 
+		c); return 0;
+}
+*/
 
-	printf("%d의 거듭제곱 : %d", num, num * num);
-
+//2번
+#include <stdio.h>
+int main(void) {
+	int a=0, b;
+	float c = 3.14;
+	printf("input a(integer value) :");
+	scanf("%d", &a);
+	printf("Radius : %d\n", a);
+	b = 2 * a;
+	printf("Diameter : %d\n", b);
+	printf("Circumstance : %.2f\n", b * c);
 	return 0;
 }
